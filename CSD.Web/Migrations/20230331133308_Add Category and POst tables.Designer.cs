90863745ef45edafb6a6ea36cf1180eb74087afe
Pe6e3 <xpe6e3@gmail.com>
2023-03-31 19:34:24 +0600

    Добавил миграции и создал БД

--- CSD.Web/Migrations/20230331133308_Add Category and POst tables.Designer.cs
+++ CSD.Web/Migrations/20230331133308_Add Category and POst tables.Designer.cs
@@ -4,6 +4,7 @@ using CSD.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CSD.Web.Migrations
 {
     [DbContext(typeof(CSDContext))]
-    partial class CSDContextModelSnapshot : ModelSnapshot
+    [Migration("20230331133308_Add Category and POst tables")]
+    partial class AddCategoryandPOsttables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
